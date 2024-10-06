2d832110d5b0867d572ba2f105bffa81e4ede9ff
JrHero14 <Overload.emperor14@gmail.com>
2024-10-06 14:18:55 +0700

    Refactor: + Add PHP Docs

--- app/Http/Repositories/User/UserRepositoryImplementation.php
+++ app/Http/Repositories/User/UserRepositoryImplementation.php
@@ -10,21 +10,40 @@ use Illuminate\Support\Facades\Hash;
 class UserRepositoryImplementation implements UserRepository
 {
     private $userModel;
+
+    /**
+     * @param User $userModel
+     */
     public function __construct(User $userModel)
     {
         $this->userModel = $userModel;
     }
 
+    /**
+     * @param int $id
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model|object|null
+     */
     public function geById(int $id)
     {
         return $this->userModel::query()->where('id', $id)->first();
     }
 
+    /**
+     * @param string $email
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model|object|null
+     */
     public function getByEmail(string $email)
     {
         return $this->userModel::query()->where('email', $email)->first();
     }
 
+    /**
+     * @param string $name
+     * @param string $email
+     * @param string $password
+     * @return void
+     * @throws FailedResponse
+     */
     public function createUser(string $name, string $email, string $password)
     {
         // Create New User
